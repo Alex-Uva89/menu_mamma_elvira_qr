4eee49669a8026e1349fd2332df7ba42e5f10e16
Alex-Uva89 <a.uva1988@gmail.com>
2024-08-12 12:37:19 +0200

    chore: Update dish card styling and fix allergen display

--- frontend_menu_qr/src/views/ViewDish.vue
+++ frontend_menu_qr/src/views/ViewDish.vue
@@ -1,13 +1,40 @@
 <template>
   <div>
+    <header :style="{ background: `var(--nav-${venueName.replace(/\s+/g, '-').replace(/,/g, '').replace(/'/g, '')})` }" class="header-dish">
+      <img :src="`/assets/${venueName}.svg`" alt="">
+    </header>
     <img :src="dish.image" alt="dish Image">
-    <h1>{{ dish.name }}</h1>
-    <p>{{ dish.description }}</p>
-    <p>Locale: {{ venueName }}</p>
+    <div class="dish-info">
+
+      <h1>{{ dish.name }}</h1>
+      <p :style="{ background: `var(--nav-${venueName.replace(/\s+/g, '-').replace(/,/g, '').replace(/'/g, '')})` }">Ingredienti</p>
+      <span>{{ dish.description }}</span>
+      <p :style="{ background: `var(--nav-${venueName.replace(/\s+/g, '-').replace(/,/g, '').replace(/'/g, '')})` }">abbinamenti</p>
+      <span></span>
+      <p :style="{ background: `var(--nav-${venueName.replace(/\s+/g, '-').replace(/,/g, '').replace(/'/g, '')})` }">i consigli della mamma</p>
+      <span></span>
+      <p :style="{ background: `var(--nav-${venueName.replace(/\s+/g, '-').replace(/,/g, '').replace(/'/g, '')})` }" class="allergens-dish">
+        allergeni
+      </p>
+      <ul class="allergens-list">
+        <template v-for="allergen in filteredAllergens">
+          <li>
+            <img :src="allergen.icon" alt="">
+          </li>
+        </template>
+      </ul>
+    </div>
+    <footer :style="{ background: `var(--nav-${venueName.replace(/\s+/g, '-').replace(/,/g, '').replace(/'/g, '')})` }" class="footer-dish">
+      <a href="http://mammaelvira.com/">
+        <img src="../../public/assets/mammaelvira_2024.svg" alt="">
+      </a>
+    </footer>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'ViewDish',
   props: {
@@ -22,7 +49,37 @@ export default {
     venueName: {
       type: String,
       required: true
+    },
+    allergensDish: {
+      type: Object,
+      required: true
     }
-  }
+  },
+  data() {
+    return {
+      allergens: []
+    };
+  },
+  created() {
+    this.fetchAllergens();
+  },
+  methods: {
+    fetchAllergens() {
+      axios.get(`${process.env.VUE_APP_API_URL}/api/allergens`)
+        .then(response => {
+          this.allergens = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  },
+  computed: {
+    filteredAllergens() {
+      return this.allergens.filter(allergen => 
+        this.allergensDish.some(ad => ad.allergen_id === allergen.id && ad.dish_id === this.dish.id)
+      );
+    }
+  },
 };
 </script>
