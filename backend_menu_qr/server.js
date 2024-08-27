ccae092bcfac5669da6c316471c723557edeb84d
Alex-Uva89 <a.uva1988@gmail.com>
2024-08-27 10:37:13 +0200

    Update dish card to handle null image values and display additional drink details

--- backend_menu_qr/server.js
+++ backend_menu_qr/server.js
@@ -65,6 +65,7 @@ app.get('/api/venues/:venue', (req, res) => {
         d.name_en AS dish_name_en,
         d.image AS dish_image,
         d.description AS dish_description,
+        d.description_en AS dish_description_en,
         d.price AS dish_price,
         d.is_active AS dish_is_active,
         dr.id AS drink_id, 
@@ -112,6 +113,7 @@ app.get('/api/venues/:venue', (req, res) => {
           name_en: row.dish_name_en,
           image: row.dish_image,
           description: row.dish_description,
+          description_en: row.dish_description_en,
           price: row.dish_price,
           is_active: row.dish_is_active,
         });
